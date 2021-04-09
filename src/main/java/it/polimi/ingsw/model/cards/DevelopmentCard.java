49973f2fe0111815ad763b3ec12210aff5b24e5e
Manuel <manuelpolimi@outlook.it>
2021-04-09 14:20:43 +0200

    Updated Card, DevelopmentCard and SpecialAbility classes. Created DevelopmentCardTest.
    Classes updated are Card, DevelopmentCard (with its Test) and SpecialAbility classes: Discount, ExtraDeposit, ExtraProduction, ExtraResource and SpecialAbility interface.

--- src/main/java/it/polimi/ingsw/model/cards/DevelopmentCard.java
+++ src/main/java/it/polimi/ingsw/model/cards/DevelopmentCard.java
@@ -1,15 +1,29 @@
 package it.polimi.ingsw.model.cards;
 
+import it.polimi.ingsw.model.exceptions.NegativeResourceValueException;
 import it.polimi.ingsw.model.resources.Resources;
 import jdk.jshell.spi.ExecutionControl.NotImplementedException;
 
+/**
+ * DevelopmentCard is a subclass of Card, its aim is to produce new resources.
+ * It has a cost and rules for its production
+ */
 public class DevelopmentCard extends Card{
-    private final Resources cost;
-    private final char type;
-    private final int level;
-    private final Resources productionCost;
-    private final Resources productionPower;
+    private final Resources cost;   //in the top of card, number and type of resource required to obtain DevelopmentCard
+    private final char type;    //b = blue, g = green, p = purple, y = yellow
+    private final int level;    //number of dots in the flags
+    private final Resources productionCost;    //resources in the left page of book, to give
+    private final Resources productionPower;   //resources in the right page of book, to receive
 
+    /**
+     * Constructor of DevelopmentCard class
+     * @param victoryPoint Victory Point of Card
+     * @param cost cost in term of resources to buy the Card
+     * @param type color of flag's Card, b = blue, g = green, p = purple, y = yellow
+     * @param level number of dots in flag's Card (if none, level = 1)
+     * @param productionCost resources needed for production
+     * @param productionPower resources obtained from production
+     */
     public DevelopmentCard(int victoryPoint, Resources cost, char type, int level, Resources productionCost, Resources productionPower) {
         super(victoryPoint);
         this.cost = cost;
@@ -19,15 +33,50 @@ public class DevelopmentCard extends Card{
         this.productionPower = productionPower;
     }
 
+    /**
+     * Getter of type
+     * @return type (color) of DevelopmentCard
+     */
+    public char getType() {
+        return type;
+    }
+
+    /**
+     * Getter of Level
+     * @return level (number of dots in flags, if none --> level = 1) of DevelopmentCard
+     */
+    public int getLevel() {
+        return level;
+    }
+
+    /**
+     * Getter of production cost
+     * @return resources needed for production
+     */
     public Resources getProductionCost() {
         return productionCost;
     }
 
+    /**
+     * Getter of production power
+     * @return resources obtain from production
+     */
     public Resources getProductionPower() {
         return productionPower;
     }
 
-    public Boolean checkAvailability(Resources resource) throws NotImplementedException {
-        throw new NotImplementedException("NON IMPLEMENTATO");
+    /**
+     * Check if is it possible to use the production of the DevelopmentCard
+     * @param resource resources available for the player to produce
+     * @return boolean true or false possibility to use that one DevelopmentCard with that resources available
+     */
+    public Boolean checkAvailability(Resources resource) {
+        try{
+            resource.sub(getProductionCost());
+            return true;
+        }
+        catch (NegativeResourceValueException ex) {
+            return false;
+        }
     }
 }
--- src/main/java/it/polimi/ingsw/model/cards/specialAbility/ExtraDeposit.java
+++ src/main/java/it/polimi/ingsw/model/cards/specialAbility/ExtraDeposit.java
@@ -2,14 +2,27 @@ package it.polimi.ingsw.model.cards.specialAbility;
 
 import it.polimi.ingsw.model.resources.ResourceTypes;
 
+/**
+ * A specialAbility that gives a new special 2-slot depot that must contains the resource indicated.
+ * Player can fill it with resources that he already have in other level of depot.
+ */
 public class ExtraDeposit implements SpecialAbility{
     private final ResourceTypes resourceType;
 
+    /**
+     * Constructor of ExtraDeposit
+     * @param resourceType type of resource that can be store in the extra 2-slot of depot
+     */
     public ExtraDeposit(ResourceTypes resourceType) {
         this.resourceType = resourceType;
     }
 
+    /**
+     * Getter of resourceType for ExtraDeposit
+     * @return the type of resource that can be store in the extra 2-slot of depot
+     */
     public ResourceTypes getResourceType() {
         return resourceType;
     }
+
 }
