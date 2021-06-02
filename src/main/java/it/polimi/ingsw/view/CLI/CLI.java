81462093a4cc42286ab9886f329e4126b2472591
Manuel <manuelpolimi@outlook.it>
2021-06-02 14:00:54 +0200

    Updated showWarehouse for ExtraDeposit.

--- src/main/java/it/polimi/ingsw/view/CLI/CLI.java
+++ src/main/java/it/polimi/ingsw/view/CLI/CLI.java
@@ -17,6 +17,7 @@ import it.polimi.ingsw.model.resources.Resources;
 import it.polimi.ingsw.view.NetworkHandler;
 import it.polimi.ingsw.view.View;
 
+import javax.swing.*;
 import java.util.*;
 import java.util.regex.*;
 
@@ -590,6 +591,7 @@ public class CLI extends View{
                         }
 
 
+
                         resourceMovedCorrectly();
                         break;
                     }
@@ -722,9 +724,12 @@ public class CLI extends View{
                     System.out.print(BLANK + " ");
             }
             else {
-                for (int j = 1; j <= i; j++) {
-                    System.out.print(BLANK + " ");
-                }
+                if(0 < i && i < 4) {
+                    for (int j = 1; j <= i; j++)
+                        System.out.print(BLANK + " ");
+                }else
+                    for (int j = 1; j <= 2; j++)
+                        System.out.println(BLANK + " ");
             }
             System.out.print("\n");
         }
