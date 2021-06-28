fa4a71f9912add65d18e31f6f1d5a22b84b69539
Luca Molteni <luca9.molteni@mail.polimi.it>
2021-06-28 23:37:56 +0200

    Purged waitAndGetResponse

--- src/main/java/it/polimi/ingsw/view/GUI/GUIView.java
+++ src/main/java/it/polimi/ingsw/view/GUI/GUIView.java
@@ -3,14 +3,15 @@ package it.polimi.ingsw.view.GUI;
 import it.polimi.ingsw.network.NetworkMessages;
 import it.polimi.ingsw.network.ObjectUpdate;
 import it.polimi.ingsw.view.GUI.Controllers.Board.GameBoardController;
-import it.polimi.ingsw.view.GUI.Controllers.CardDealerController;
 import it.polimi.ingsw.view.View;
 
 import java.io.IOException;
 
 import static it.polimi.ingsw.network.NetworkMessages.*;
 
-
+/**
+ * Class that represents a view of the server for a GUI
+ */
 public class GUIView extends View {
     public static GUIView singleton;
     public String lastErrorMessage;
@@ -18,9 +19,15 @@ public class GUIView extends View {
     public boolean isMyTurn = false;
 
     /**
-     * Method that receives a message and check if it is a success or an error.
-     * If  a success is received it also waits for the game update, if an error is received error message is printed
-     * @return true or false depending on the message received: SUCCESS --> true, ERROR --> false
+     * Private constructor for singleton purposes
+     */
+    private GUIView(){
+
+    }
+
+    /**
+     * Method that handles the server messages and notifies the GUI in case of an error
+     * @return True if is success and false if error
      */
     @Override
     public boolean isSuccessReceived() {
@@ -40,11 +47,18 @@ public class GUIView extends View {
         }
     }
 
+    /**
+     * Method that notifies this object of a victory
+     * @param youWon True if you won and false if not
+     */
     @Override
     protected void notifyEndGame(boolean youWon) {
         gameBoardController.handleGameEnd(youWon);
     }
 
+    /**
+     * Method that notifies this object that the turn has started
+     */
     @Override
     public synchronized void notifyTurnStarted() {
         while (gameBoardController==null){
@@ -58,6 +72,9 @@ public class GUIView extends View {
         gameBoardController.startTurn();
     }
 
+    /**
+     * Method that notifies this object that the turn has ended
+     */
     @Override
     public void notifyTurnEnded() {
         while (gameBoardController==null){
@@ -71,17 +88,28 @@ public class GUIView extends View {
         gameBoardController.endTurn();
     }
 
+    /**
+     * Method that notifies this object that the server has disconnected
+     */
     @Override
     public void notifyDisconnection() {
         if(this.gameBoardController!=null)
             this.gameBoardController.handleDisconnect();
     }
 
+    /**
+     * Method used to register the main controller for notification purposes
+     * @param gameBoardController The main controller
+     */
     public synchronized void registerStage(GameBoardController gameBoardController){
         this.gameBoardController=gameBoardController;
         notifyAll();
     }
 
+    /**
+     * Singleton method
+     * @return The object instance
+     */
     public static GUIView singleton(){
         if(singleton == null){
             singleton = new GUIView();
@@ -89,6 +117,12 @@ public class GUIView extends View {
         return singleton;
     }
 
+    /**
+     * Method that creates a game and gets the game id
+     * @param numberOfPlayers The number of players
+     * @return The game id
+     * @throws IOException In case there's a problem with the communication
+     */
     public String createGameAndGetId(int numberOfPlayers) throws IOException {
         super.createGame(numberOfPlayers);
         if(isSuccessReceived())
@@ -97,7 +131,12 @@ public class GUIView extends View {
             throw new IOException();
     }
 
-
+    /**
+     * Choose the initial leader cards
+     * @param chose The leader selected
+     * @return True if successful and false if not
+     * @throws IOException In case there's a problem with the communication
+     */
     public boolean chooseLeaderAndWaitForStart(Integer[] chose) throws IOException {
         super.chooseLeader(chose);
         if(isSuccessReceived()) {
@@ -106,6 +145,10 @@ public class GUIView extends View {
         return false;
     }
 
+    /**
+     * Method that waits for a new response from the server
+     * @return The response object
+     */
     protected Object waitAndGetResponse(){
         Object ret = super.waitAndGetResponse();
         if(ret==TURNBEGIN || ret==TURNEND)
@@ -114,6 +157,10 @@ public class GUIView extends View {
             return ret;
     }
 
+    /**
+     * Method that notifies this view of a new update
+     * @param read The update
+     */
     @Override
     public void notifyNewUpdate(ObjectUpdate read) {
         super.notifyNewUpdate(read);
--- src/main/java/it/polimi/ingsw/view/View.java
+++ src/main/java/it/polimi/ingsw/view/View.java
@@ -1,6 +1,5 @@
 package it.polimi.ingsw.view;
 
-
 import it.polimi.ingsw.controller.Game;
 import it.polimi.ingsw.model.cards.LeaderCard;
 import it.polimi.ingsw.model.resources.ResourceTypes;
@@ -16,20 +15,29 @@ import java.util.Queue;
 
 import static it.polimi.ingsw.network.NetworkMessages.*;
 
+
+/**
+ * Class that represents a view of the game on the server
+ */
 public abstract class View {
     private NetworkHandler networkHandler;
     public Game game;
-    public int playerNumber; //the number of the player received before GAMESTARTED
+    public int playerNumber; //The number of the player
 
     public final Queue<Object> messages = new LinkedList<>(); //List of received messages
     protected boolean gameUpdated = false;
 
-    protected abstract boolean isSuccessReceived();
-
+    /**
+     * Method that closes connection with the server
+     */
     public void closeConnection() {
         networkHandler.closeConnection();
     }
 
+    /**
+     * Method that notifies a response to this view
+     * @param o The response
+     */
     public synchronized void notifyResponse(Object o) {
         if (o.getClass() == NetworkMessages.class)
             if (o == SUCCESS)
@@ -38,6 +46,10 @@ public abstract class View {
         this.notifyAll();
     }
 
+    /**
+     * Method that notifies this object of a new game
+     * @param game The new game
+     */
     public void notifyNewGame(Game game) {
         updateObjects(game);
     }
@@ -86,8 +98,10 @@ public abstract class View {
         return messages.remove();
     }
 
-    protected abstract void notifyEndGame(boolean youWon);
-
+    /**
+     * Method that notifies this view of a new update
+     * @param read The update
+     */
     public synchronized void notifyNewUpdate(ObjectUpdate read) {
         switch (read.getUpdateType()){
             case LEADERCARDS:
@@ -101,55 +115,109 @@ public abstract class View {
         this.notifyAll();
     }
 
-
+    /**
+     * Method that sets the initial resources
+     * @param res1 The first and only selectable resource
+     * @throws IOException In case there's a problem with the communication
+     */
     public void setInitialResources(ResourceTypes res1) throws IOException {
         networkHandler.sendObject(res1);
     }
 
+    /**
+     * Method that sets the initial resources
+     * @param res1 The first resource
+     * @param res2 The second resource
+     * @throws IOException In case there's a problem with the communication
+     */
     public void setInitialResources(ResourceTypes res1, ResourceTypes res2) throws IOException {
         networkHandler.sendObject(res1);
         networkHandler.sendObject(res2);
     }
 
+    /**
+     * Choose the initial leader cards
+     * @param chose The selected leader cards
+     * @throws IOException In case there's a problem with the communication
+     */
     public void chooseLeader(Integer[] chose) throws IOException {
         networkHandler.sendObject(chose);
     }
 
-    public void discardLeader(int currentCard) throws IOException {
+    /**
+     * Method to discard a leader card
+     * @param card The card to be discarded
+     * @throws IOException In case there's a problem with the communication
+     */
+    public void discardLeader(int card) throws IOException {
         networkHandler.sendObject(DISCARDLEADER);
-        networkHandler.sendObject(currentCard);
+        networkHandler.sendObject(card);
     }
 
-    public void activateLeader(int currentCard) throws IOException {
+    /**
+     * Method to activate a leader card
+     * @param card The card to be activated
+     * @throws IOException In case there's a problem with the communication
+     */
+    public void activateLeader(int card) throws IOException {
         networkHandler.sendObject(ACTIVATELEADER);
-        networkHandler.sendObject(currentCard);
+        networkHandler.sendObject(card);
     }
 
+    /**
+     * Method to end the production
+     * @throws IOException In case there's a problem with the communication
+     */
     public void endProduction() throws IOException {
         networkHandler.sendObject(ENDPRODUCTION);
     }
 
-    public void productionProd1(ResourceTypes res1, ResourceTypes res2, ResourceTypes res3) throws IOException {
+    /**
+     * Method that uses the default production
+     * @param res1 The first input resource
+     * @param res2 The second input resource
+     * @param output The output resource
+     * @throws IOException In case there's a problem with the communication
+     */
+    public void productionProd1(ResourceTypes res1, ResourceTypes res2, ResourceTypes output) throws IOException {
         networkHandler.sendObject(PRODUCTION);
         networkHandler.sendObject(PROD1);
         networkHandler.sendObject(res1);
         networkHandler.sendObject(res2);
-        networkHandler.sendObject(res3);
+        networkHandler.sendObject(output);
     }
 
+    /**
+     * Method that uses a card production
+     * @param currentCard The card to use in this production
+     * @throws IOException In case there's a problem with the communication
+     */
     public void productionProd2(int currentCard) throws IOException {
         networkHandler.sendObject(PRODUCTION);
         networkHandler.sendObject(PROD2);
         networkHandler.sendObject(currentCard);
     }
 
-    public void productionProd3(ResourceTypes res1, ResourceTypes res2) throws IOException {
+    /**
+     * Method that uses a leader production
+     * @param input The input resource
+     * @param output The output resource
+     * @throws IOException In case there's a problem with the communication
+     */
+    public void productionProd3(ResourceTypes input, ResourceTypes output) throws IOException {
         networkHandler.sendObject(PRODUCTION);
         networkHandler.sendObject(PROD3);
-        networkHandler.sendObject(res1);
-        networkHandler.sendObject(res2);
+        networkHandler.sendObject(input);
+        networkHandler.sendObject(output);
     }
 
+    /**
+     * Method that buys a card from the card dealer
+     * @param row The row of the card
+     * @param column The column of the card
+     * @param place The place to insert the card
+     * @throws IOException In case there's a problem with the communication
+     */
     public void marketBuyCard(int row, int column, int place) throws IOException {
         networkHandler.sendObject(BUYCARD);
         networkHandler.sendObject(row);
@@ -157,11 +225,23 @@ public abstract class View {
         networkHandler.sendObject(place);
     }
 
+    /**
+     * Method that moves a level to the swap
+     * @param level The level to be moved
+     * @throws IOException In case there's a problem with the communication
+     */
     public void swapMoveToSwap(int level) throws IOException {
         networkHandler.sendObject(MOVETOSWAP);
         networkHandler.sendObject(level);
     }
 
+    /**
+     * Method that moves resources to the level
+     * @param level The level to move the resources
+     * @param resourceTypesToMove The type of resources
+     * @param numResToMove The number of resources
+     * @throws IOException In case there's a problem with the communication
+     */
     public void swapMoveToLevel(int level, ResourceTypes resourceTypesToMove, int numResToMove) throws IOException {
         networkHandler.sendObject(MOVETOLEVEL);
         networkHandler.sendObject(level);
@@ -169,22 +249,45 @@ public abstract class View {
         networkHandler.sendObject(numResToMove);
     }
 
+    /**
+     * Method that drop the swap resources
+     * @throws IOException In case there's a problem with the communication
+     */
     public void swapDropResources() throws IOException {
         networkHandler.sendObject(DROPRESOURCES);
     }
 
+    /**
+     * Method that buys a column from the market
+     * @param column The column to buy
+     * @param extraResourceIndex The extra resource effect
+     * @throws IOException In case there's a problem with the communication
+     */
     public void marketBuyColumn(int column, int extraResourceIndex) throws IOException {
         networkHandler.sendObject(BUYCOLUMN);
         networkHandler.sendObject(column);
         networkHandler.sendObject(extraResourceIndex);
     }
 
+    /**
+     * Method that buys a row from the market
+     * @param row The row to buy
+     * @param extraResourceIndex The extra resource effect
+     * @throws IOException In case there's a problem with the communication
+     */
     public void marketBuyRow(int row, int extraResourceIndex) throws IOException {
         networkHandler.sendObject(BUYROW);
         networkHandler.sendObject(row);
         networkHandler.sendObject(extraResourceIndex);
     }
 
+    /**
+     * Method that sends the nickname to the server
+     * @param nickname The nickname
+     * @throws IOException In case there's a problem with the communication
+     * @throws UnknownIdException In case the room doesn't exist
+     * @throws FullRoomException In case the room is full
+     */
     public void sendNickname(String nickname) throws IOException, UnknownIdException, FullRoomException {
         networkHandler.sendObject(nickname);
         Object o = waitAndGetResponse();
@@ -200,28 +303,69 @@ public abstract class View {
         }
     }
 
+    /**
+     * Method that starts a connection with the server
+     * @param serverAddress The address of the server
+     * @param serverPort The port of the server
+     * @throws IOException In case there's a problem with the communication
+     */
     public void startConnection(String serverAddress, int serverPort) throws IOException {
         networkHandler = new NetworkHandler(serverAddress,serverPort,this);
         networkHandler.start();
     }
 
+    /**
+     * Method that joins a game
+     * @param roomId The room id of the game to join
+     * @throws IOException In case there's a problem with the communication
+     */
     public void joinGame(String roomId) throws IOException {
         networkHandler.sendObject(JOINGAME);
         networkHandler.sendObject(roomId);
     }
 
+    /**
+     * Method that creates a game
+     * @param numberOfPlayers The number of players of this game
+     * @throws IOException In case there's a problem with the communication
+     */
     public void createGame(int numberOfPlayers) throws IOException {
         networkHandler.sendObject(CREATEGAME);
         networkHandler.sendObject(numberOfPlayers);
     }
 
+    /**
+     * Ends the turn
+     * @throws IOException In case there's a problem with the communication
+     */
     public void endTurn() throws IOException {
         networkHandler.sendObject(TURNEND);
     }
 
+    /**
+     * Method that handles the server messages
+     * @return True if is success and false if error
+     */
+    protected abstract boolean isSuccessReceived();
+
+    /**
+     * Method that notifies this object of a victory
+     * @param youWon True if you won and false if not
+     */
+    protected abstract void notifyEndGame(boolean youWon);
+
+    /**
+     * Method that notifies this object that the turn has started
+     */
     public abstract void notifyTurnStarted();
 
+    /**
+     * Method that notifies this object that the turn has ended
+     */
     public abstract void notifyTurnEnded();
 
+    /**
+     * Method that notifies this object that the server has disconnected
+     */
     public abstract void notifyDisconnection();
 }
